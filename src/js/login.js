30acc942a771750356beddacaf5f042b50c7e4e9
Gormm <juan.cornejo.s@gmail.com>
2024-03-03 18:59:22 -0300

    .

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,8 +11,7 @@ const firebaseConfig = {
   appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Función para iniciar sesión
 function signInWithGoogle() {
