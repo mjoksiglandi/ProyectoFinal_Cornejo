ec79d59080dc4774845f6535dc3cb48692200046
Gormm <juan.cornejo.s@gmail.com>
2024-03-04 20:18:08 -0300

    .

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
