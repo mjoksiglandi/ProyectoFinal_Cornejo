730c66964997e8b607772a00f79c7505fb6e68ff
Gormm <juan.cornejo.s@gmail.com>
2024-03-08 20:07:00 -0300

    new config

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,12 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
-    authDomain: "trashpandagarage-4abd6.firebaseapp.com",
-    databaseURL: "https://trashpandagarage-4abd6-default-rtdb.firebaseio.com",
-    projectId: "trashpandagarage-4abd6",
-    storageBucket: "trashpandagarage-4abd6.appspot.com",
-    messagingSenderId: "1010689914185",
-    appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
-  };
-  
-  
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
+  authDomain: "trashpandagarage-4abd6.firebaseapp.com",
+  databaseURL: "https://trashpandagarage-4abd6-default-rtdb.firebaseio.com",
+  projectId: "trashpandagarage-4abd6",
+  storageBucket: "trashpandagarage-4abd6.appspot.com",
+  messagingSenderId: "1010689914185",
+  appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
