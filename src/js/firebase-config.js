05b6ef8ed570c56e44e0fb9913d8768ecec54081
Gormm <juan.cornejo.s@gmail.com>
2024-03-08 19:59:02 -0300

    try 4

---
+++ src/js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
+    authDomain: "trashpandagarage-4abd6.firebaseapp.com",
+    databaseURL: "https://trashpandagarage-4abd6-default-rtdb.firebaseio.com",
+    projectId: "trashpandagarage-4abd6",
+    storageBucket: "trashpandagarage-4abd6.appspot.com",
+    messagingSenderId: "1010689914185",
+    appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
+  };
+  
+  
+  firebase.initializeApp(firebaseConfig);
--- src/js/login.js
+++ src/js/login.js
@@ -1,58 +1,20 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
-  authDomain: "trashpandagarage-4abd6.firebaseapp.com",
-  databaseURL: "https://trashpandagarage-4abd6-default-rtdb.firebaseio.com",
-  projectId: "trashpandagarage-4abd6",
-  storageBucket: "trashpandagarage-4abd6.appspot.com",
-  messagingSenderId: "1010689914185",
-  appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
-};
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
 
 
-firebase.initializeApp(firebaseConfig);
 
-// Obtén una referencia al servicio de autenticación
-const auth = firebase.auth().signInWithPopup();
-
-// Función para iniciar sesión
-function login() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  
-  auth.signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-          // Usuario inició sesión correctamente
-          const user = userCredential.user;
-          console.log('Usuario autenticado:', user);
+// Función para iniciar sesión con Google
+function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        // Se ha iniciado sesión correctamente
+        const user = result.user;
+        console.log('Inicio de sesión exitoso:', user);
       })
       .catch((error) => {
-          // Error al iniciar sesión
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.error('Error al iniciar sesión:', errorMessage);
+        // Se ha producido un error
+        console.error('Error al iniciar sesión:', error);
       });
-}
-
-// Función para registrarse
-function registrar() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  
-  auth.createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-          // Usuario registrado correctamente
-          const user = userCredential.user;
-          console.log('Usuario registrado:', user);
-      })
-      .catch((error) => {
-          // Error al registrar usuario
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.error('Error al registrar usuario:', errorMessage);
-      });
-}
+  }
